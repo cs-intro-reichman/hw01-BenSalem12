a889d15c3bfe66f3c46b2ddb598b656de5ec9ba7
BenSalem12 <154272684+BenSalem12@users.noreply.github.com>
2023-12-24 01:00:15 +0200

    Add files via upload

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,14 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int max = Integer.parseInt (args [0]);
+		int min = Integer.parseInt (args [1]);
+		int first_num = (int)(Math.random()*(max - min) ) + min;
+		int sec_num = (int)(Math.random()*(max - min) ) + min;
+		int third_num = (int)(Math.random()*(max - min) ) + min;
+		int temp_min = Math.min (first_num, sec_num);
+		int final_min = Math.min (temp_min, third_num);
+		System.out.println(first_num + "\n" + sec_num + "\n" + third_num);
+		System.out.println("The minimal generated number was " + final_min); 		
 	}
 }
--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,10 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt (args[0]);
+		int b = Integer.parseInt (args[1]);
+		int c = Integer.parseInt (args[2]);
+		boolean if_Triangle =  a + b > c && a + c > b && b + c > a ;
+		System.out.println (a + ", " + b + ", " + c + ": " + if_Triangle);
 	}
 }
