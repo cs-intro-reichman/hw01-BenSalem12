d9e289cd459c35f20c55240adfbf2434795b9ce8
BenSalem12 <154272684+BenSalem12@users.noreply.github.com>
2023-12-24 02:07:34 +0200

    Add files via upload

--- GenThree.java
+++ GenThree.java
@@ -4,14 +4,14 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		int max = Integer.parseInt (args [0]);
-		int min = Integer.parseInt (args [1]);
+		int min = Integer.parseInt (args [0]);
+		int max = Integer.parseInt (args [1]);
 		int first_num = (int)(Math.random()*(max - min) + min);
 		int sec_num = (int)(Math.random()*(max - min) + min);
 		int third_num = (int)(Math.random()*(max - min) + min);
 		int temp_min = Math.min (first_num, sec_num);
 		int final_min = Math.min (temp_min, third_num);
 		System.out.println(first_num + "\n" + sec_num + "\n" + third_num);
-		System.out.println("The minimal generated number was " + final_min); 		
+		System.out.println("The minimal generated number was " + final_min);		
 	}
 }
